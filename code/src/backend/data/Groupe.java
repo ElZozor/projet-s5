5508d24ada8a4bd83a5c9364826cb5e49899a7dd
zozomomo31 <44607219+zozomomo31@users.noreply.github.com>
2020-01-07 12:17:53 +0100

    Update Groupe.java

--- code/src/backend/data/Groupe.java
+++ code/src/backend/data/Groupe.java
@@ -21,8 +21,8 @@ public class Groupe extends ProjectTable implements Comparable<Groupe> {
     /**
      * Constructeur de l'objet Groupe à partir d'un identifiant et d'un nom de groupe
      *
-     * @param id : identifiant unique du groupe 
-     * @param label : nom du groupe
+     * @param id - identifiant unique du groupe 
+     * @param label - nom du groupe
      **/
     public Groupe(final Long id, final String label) {
         mID = id;
@@ -32,9 +32,9 @@ public class Groupe extends ProjectTable implements Comparable<Groupe> {
     /**
      * Constructeur de l'objet Groupe à partir d'un identifiant, d'un nom de groupe et d'un ensemble trié de tickets
      *
-     * @param id : identifiant unique du groupe 
-     * @param label : nom du groupe
-     * @param tickets : ensemble de tickets liés à ce groupe
+     * @param id - identifiant unique du groupe 
+     * @param label - nom du groupe
+     * @param tickets - ensemble de tickets liés à ce groupe
     **/
     public Groupe(final Long id, final String label, final TreeSet<Ticket> tickets) {
         mID = id;
@@ -45,8 +45,8 @@ public class Groupe extends ProjectTable implements Comparable<Groupe> {
     /**
      * Constructeur de l'objet Groupe à partir des informations extraites de la base de données
      *
-     * @param set : ensemble d'informations recupérées dans la base de donnée
-     * @throws SQLException : peut être renvoyée si une entrée n'est pas présente dans le ResultSet
+     * @param set - ensemble d'informations recupérées dans la base de donnée
+     * @throws SQLException - peut être renvoyée si une entrée n'est pas présente dans le ResultSet
     **/
     public Groupe(ResultSet set) throws SQLException {
         mID = set.getLong(1);
@@ -56,7 +56,7 @@ public class Groupe extends ProjectTable implements Comparable<Groupe> {
     /**
      * Constructeur de l'objet Groupe à partir d'un message au format JSON 
      *
-     * @param jsonObject : objet json contenant toutes les onformations d'un groupe
+     * @param jsonObject - objet json contenant toutes les onformations d'un groupe
     **/
     public Groupe(JSONObject jsonObject) {
         mID = jsonObject.getLong(GROUPE_ID);
@@ -71,38 +71,73 @@ public class Groupe extends ProjectTable implements Comparable<Groupe> {
     }
     
     /**
-     * Constructeur de l'objet groupe à partir d'un titre 
+     * Constructeur de l'objet groupe à partir de son nom
+     *
+     *  @param label - nom du groupe a creer
     **/
     public Groupe(String label) {
         super();
 
         setLabel(label);
     }
-
+    
+    /**
+     * Accesseur sur l'identifiant du groupe
+     *
+     * @return identifiant du groupe
+    **/
     public Long getID() {
         return mID;
     }
-
+    
+    /**
+     * Mutateur sur l'identifiant du groupe
+     *
+     * @param id - nouvel identifiant
+    **/
     public void setID(Long id) {
         mID = id;
     }
-
+    
+    /**
+     * Accesseur sur le nom du groupe
+     *
+     * @return nom du groupe
+    **/
     public String getLabel() {
         return mLabel;
     }
-
+    
+    /**
+     * Mutateur sur le nom du groupe
+     *
+     * @param label - nouveau nom du groupe
+    **/
     public void setLabel(String label) {
         mLabel = label;
     }
-
+    /**
+     * Methode ajoutant un ticket au groupe
+     * @param ticket - Ticket à ajouter à l'nesemble de tickets du groupe
+    **/ 
     public void addTicket(Ticket ticket) {
         mTickets.add(ticket);
     }
 
+    /**
+     * Accesseur sur l'ensemble de tickets du groupe
+     *
+     * @return l'ensemble de tickets du groupe
+    **/ 
     public TreeSet<Ticket> getTickets() {
         return mTickets;
     }
-
+    
+    /**
+     * Methode qui encode un groupe en un objet json 
+     *
+     * @return un objet au format JSON contenant toutes les informations du groupe
+    **/
     public JSONObject toJSON() {
         JSONObject jsonObject = new JSONObject();
 
